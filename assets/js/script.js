be9348d9b55ed56909b2bd3823c713eb59146fb2
wisnuragaprawida <wisnuraga418@gmail.com>
2022-08-25 23:14:13 +0700

    [feat] create simple html css js

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+function loadAnime() {
+  fetch('../../stack.json')
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('data-anime').innerHTML = "";
+
+      const json = JSON.stringify(data.anime);
+      const result = JSON.parse(json);
+
+      for (let i = 0; i < result.length; i++) {
+        document.getElementById('data-anime').innerHTML += `
+      <div class="card">
+          <img src="${result[i].img}" alt="Luffy">
+          <div class="card-body">
+              <h1 class="h1-500">${result[i].name}</h1>
+              <p class="p-normal">${result[i].skill}</p>
+              <button class="btn-primary-large">Learn</button>
+          </div>
+      </div>
+      `;
+      }
+    })
+    .catch((error) => {
+      console.error(error.message);
+    });
+}
+
+loadAnime();
